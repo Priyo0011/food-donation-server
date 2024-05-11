0effe1fd46ac54075752619e1e20831aa7a5d67d
Priyo0011 <priyotosh0011@gmail.com>
2024-05-12 01:54:40 +0600

    update food

--- index.js
+++ index.js
@@ -72,6 +72,20 @@ async function run() {
       res.send(result)
     })
 
+    app.put('/Food/:id', async (req, res) => {
+      const id = req.params.id
+      const foodData = req.body
+      const query = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...foodData,
+        },
+      }
+      const result = await foodsDonation.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
